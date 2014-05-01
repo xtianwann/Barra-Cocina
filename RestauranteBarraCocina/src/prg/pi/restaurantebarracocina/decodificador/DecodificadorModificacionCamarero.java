d6f1eb20425882aaea6aa85c309df7bba6c7dc78
juanpleo1983 <juanpleo1983@gmail.com>
2014-05-01 16:48:59 +0200

    Añadido decodificador para las modificaciones del camarero

---
+++ RestauranteBarraCocina/src/prg/pi/restaurantebarracocina/decodificador/DecodificadorModificacionCamarero.java
@@ -0,0 +1,37 @@
+package prg.pi.restaurantebarracocina.decodificador;
+
+import java.util.ArrayList;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import prg.pi.restaurantebarracocina.restaurante.PedidoModificadoCamarero;
+
+public class DecodificadorModificacionCamarero {
+	private Document dom;
+	private ArrayList<PedidoModificadoCamarero> pedidosListos;
+	
+	public DecodificadorModificacionCamarero(Document dom){
+		this.dom = dom;
+		pedidosListos = new ArrayList<PedidoModificadoCamarero>();
+		generarPedidos();
+	}
+	
+	public PedidoModificadoCamarero[] getPedidosListos(){
+		return pedidosListos.toArray(new PedidoModificadoCamarero[0]);
+	}
+	
+	private void generarPedidos(){
+		NodeList nodeListPedidos = dom.getElementsByTagName("pedido");
+		for(int pedido = 0; pedido < nodeListPedidos.getLength(); pedido++){
+			Node nodePedido = nodeListPedidos.item(pedido);
+			Element elementoPedido = (Element) nodePedido;
+			int idComanda = Integer.parseInt(elementoPedido.getAttribute("idCom"));
+			int idMenu = Integer.parseInt(nodePedido.getChildNodes().item(0).getFirstChild().getNodeValue());
+			int listos = Integer.parseInt(nodePedido.getChildNodes().item(1).getFirstChild().getNodeValue());
+			pedidosListos.add(new PedidoModificadoCamarero(idComanda, idMenu, listos));
+		}
+	}
+}
